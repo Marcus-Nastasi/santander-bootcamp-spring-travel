4e832d11587a2f6898c81fd74a213d44c8fdff80
rolemberg <vinnie.nsts@gmail.com>
2024-06-03 04:04:01 -0300

    configuring response entities on UserController;

--- src/main/java/com/santander/springWeb/Controller/UserController.java
+++ src/main/java/com/santander/springWeb/Controller/UserController.java
@@ -4,6 +4,8 @@ import com.santander.springWeb.Handler.BusinessException;
 import com.santander.springWeb.Models.User;
 import com.santander.springWeb.Repository.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
@@ -23,34 +25,33 @@ public class UserController {
     }
 
     @GetMapping(value = "/api/users")
-    public List<User> getAllUsers() {
-        return userRepo.findAll();
+    public ResponseEntity<List<User>> getAllUsers() {
+        return ResponseEntity.ok(userRepo.findAll());
     }
 
     @GetMapping(value = "/api/users/{id}")
-    public User singleUser(@PathVariable("id") int id) throws BusinessException {
+    public ResponseEntity<User> singleUser(@PathVariable("id") int id) throws BusinessException {
         if (userRepo.findById(id).isEmpty()) throw new BusinessException("No value finded on this ID");
-        return userRepo.findById(id).get();
+        return ResponseEntity.ok(userRepo.findById(id).get());
     }
 
     @GetMapping(value = "/api/users/{email}/getMail")
-    public User getByEmail(@PathVariable String email) throws BusinessException {
+    public ResponseEntity<User> getByEmail(@PathVariable String email) throws BusinessException {
         if (userRepo.findByEmailAddress(email) == null) throw new BusinessException("No value finded on this email");
-        return userRepo.findByEmailAddress(email);
+        return ResponseEntity.ok(userRepo.findByEmailAddress(email));
     }
 
     @PostMapping(value = "/api/users/add")
-    public void insertUser(@RequestBody User user) throws BusinessException {
-        if (user.getName() == null
-                || user.getEmail() == null
-                || user.getBirth() == null) throw new BusinessException("Required not null values");
+    public ResponseEntity<String> insertUser(@RequestBody User user) throws BusinessException {
+        if (user.getName() == null || user.getEmail() == null || user.getBirth() == null) throw new BusinessException("Required not null values");
 
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         userRepo.save(user);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
     @PutMapping(value = "/api/users/{id}/update")
-    public void updateUser(@PathVariable("id") int id, @RequestBody User user) throws BusinessException {
+    public ResponseEntity<String> updateUser(@PathVariable("id") int id, @RequestBody User user) throws BusinessException {
         if (user.getName() == null || user.getEmail() == null || user.getBirth() == null) throw new BusinessException("Required not null values");
         if (userRepo.findById(id).isEmpty()) throw new BusinessException("User not found");
 
@@ -59,12 +60,14 @@ public class UserController {
         uFinded.update(user.getName(), user.getEmail(), user.getBirth(), pass);
 
         userRepo.save(uFinded);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
     @DeleteMapping(value = "/api/users/{id}/delete")
-    public void deleteUser(@PathVariable("id") int id) throws BusinessException {
+    public ResponseEntity<String> deleteUser(@PathVariable("id") int id) throws BusinessException {
         if(userRepo.findById(id).isPresent()) userRepo.deleteById(id);
         else throw new BusinessException("User not found.");
+        return ResponseEntity.status(HttpStatus.ACCEPTED).build();
     }
 }
 
