24040c38f8b602410059e6683c83a395aba850bb
rolemberg <vinnie.nsts@gmail.com>
2024-06-02 23:56:50 -0300

    adding SecurityFilterChain

---
+++ src/main/java/com/santander/springWeb/Infra/Security/WebSecurityConfiguration.java
@@ -0,0 +1,28 @@
+package com.santander.springWeb.Infra.Security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfiguration {
+
+    public SecurityFilterChain webSecurityConfiguration(HttpSecurity http) throws Exception {
+        return(
+            http
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(h -> {
+                    h.requestMatchers(HttpMethod.GET, "/api/users").permitAll()
+                            .anyRequest().authenticated();
+                }).build()
+        );
+    }
+}
+
+
